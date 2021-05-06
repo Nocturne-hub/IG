4603bd4976ae1228568bc83d16dbbe2c5941c50a
Nocturne-Hub <jeremy.battaglia0@gmail.com>
2021-05-06 12:26:43 +0200

    piou piou piou

---
+++ Src/Laser.cpp
@@ -0,0 +1,91 @@
+#include "Laser.h"
+
+Laser::Laser() {
+    tailleX = 0.15f;
+    tailleY = 0.15f;
+    tailleZ = 1.0f;
+}
+
+Laser::Laser(Vaisseau v, int i) {
+    tailleX = 0.15f;
+    tailleY = 0.15f;
+    tailleZ = 1.0f;
+    posX = v.coordCanons[i][0];
+    posY = v.coordCanons[i][1];
+    if (i == 0) {
+        posZ = -0.5;
+    }
+    else {
+        posZ = 1.9;
+    }
+    
+}
+
+
+float Laser::getPosX() {
+    return posX;
+}
+
+float Laser::getPosY() {
+    return posY;
+}
+
+float Laser::getPosZ() {
+    return posZ;
+}
+
+void Laser::setPosX(float x) {
+    posX = x;
+}
+
+void Laser::setPosY(float y) {
+    posY = y;
+}
+
+void Laser::setPosZ(float z) {
+    posZ = z;
+}
+void Laser::mySolidCylindre(GLdouble base, GLdouble height, GLint slices, GLint stacks) {
+    GLUquadricObj* theQuadric;
+    theQuadric = gluNewQuadric();
+    gluQuadricDrawStyle(theQuadric, GLU_FILL);
+    gluQuadricNormals(theQuadric, GLU_SMOOTH);
+    gluCylinder(theQuadric, base, base, height, slices, stacks);
+    gluDeleteQuadric(theQuadric);
+}
+
+void Laser::mySolidSphere(GLdouble height, GLint slices, GLint stacks) {
+    GLUquadricObj* sphere = NULL;
+    sphere = gluNewQuadric();
+    gluQuadricDrawStyle(sphere, GLU_FILL);
+    gluQuadricNormals(sphere, GLU_SMOOTH);
+    gluSphere(sphere, height, slices, stacks);
+    gluDeleteQuadric(sphere);
+
+}
+void Laser::mySolidLaser(){
+    glPushMatrix();
+    glRotatef(180.0f, 1.0f, 0.0f, 0.0f);
+    mySolidSphere(tailleX/2, 50, 50);
+    mySolidCylindre(tailleX / 2, tailleZ, 50, 50);
+    glTranslatef(0.0f, 0.0f, tailleZ);
+    mySolidSphere(tailleX/2, 50, 50);
+    glPopMatrix();
+}
+
+bool Laser::enCollision(Patatoide p) {
+    if (p.isBoomed()) {
+        return false;
+    }
+
+    float absX = abs(p.getPosX() - posX);
+    float absY = abs(p.getPosY() - posY);
+    float absZ = abs(p.getPosZ() - posZ);
+
+    if (absX <= tailleX && absY <= (tailleY) && absZ <= tailleZ) {
+
+        return true;
+    }
+
+    return false;
+}
---
+++ Src/Laser.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <cmath>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include "Patatoide.h"
+#include "Vaisseau.h"
+
+class Laser {
+	private:
+		float posX = 0.0f;
+		float posY = 0.0f;
+		float posZ = 0.0f;
+
+		float tailleX = 0.0f;
+		float tailleY = 0.0f;
+		float tailleZ = 0.0f;
+
+	public:
+		Laser();
+		Laser(Vaisseau v, int i);
+
+		float getPosX();
+		float getPosY();
+		float getPosZ();
+		void setPosX(float x);
+		void setPosY(float y);
+		void setPosZ(float z);
+
+		void mySolidCylindre(GLdouble base, GLdouble height, GLint slices, GLint stacks);
+		void mySolidSphere(GLdouble height, GLint slices, GLint stacks);
+		void mySolidLaser();
+
+		bool enCollision(Patatoide p);
+};
